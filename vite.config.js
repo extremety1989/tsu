02892d5a18225dbb0a97e1f3ab893890644eb303
Andranik <andreazure1989@hotmail.fr>
2023-08-11 11:00:10 +0200

    changed ignored

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import vitePluginString from "vite-plugin-string"
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig({
+    base: "/crepe-maker/",
     plugins: [vitePluginString()],
-    base: "/tsu/"
-}
+  })
+  
